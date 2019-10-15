4302d7b3178b876da9842330acfda21c55131279
HALtitor <e175753@ie.u-ryukyu.ac.jp>
2019-10-15 19:58:14 +0900

    tips装飾追加

--- src/tips.js
+++ src/tips.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import './tips.css';
+import tipsimage from './images/user.png';
 
 export class Tips extends Component {
 	render() {
 		return(
 			<div className="tips-wrapper">
 				<div className="tips">
+				<p>ユーザーストーリーマップを作ってみよう！</p>
+				<img src={tipsimage}  />
 				  <button className="closeButton"
 					           onClick={() => this.props.onClick()}>
 					           x
