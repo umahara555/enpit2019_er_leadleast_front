41cad2cd55a5ae609049cc7eb49d85229123ebdc
HALtitor <e175753@ie.u-ryukyu.ac.jp>
2019-10-19 17:27:22 +0900

    ホーム画面を作成

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import {UserStoryMap} from './UserStoryMap.js';
+import {Home} from './Home.js'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 const App = () => (
@@ -10,13 +11,6 @@ const App = () => (
       <Route path='/userstorymap' component={UserStoryMap} />    
     </div>
   </BrowserRouter>
-)
-
-const Home = () =>(
-    <div>
-    <h1>Homeです</h1>
-    <p>プロダクトデザインを支援するツールです</p>    
-    </div>
 )
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Home.css'
+
+export class Home extends Component {
+  render(){
+    return(
+      <div className='home'>
+        <h1>Homeです</h1>
+       	<p>プロダクトデザインを支援するツールです</p>    
+      </div>
+    )
+  }  
+}
