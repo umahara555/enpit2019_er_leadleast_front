d70ff746b5ea394ed18fa27fd5a250b512f3de7a
HALtitor <e175753@ie.u-ryukyu.ac.jp>
2019-10-19 17:34:28 +0900

    リーンキャンパスを作成

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import './App.css';
 import {UserStoryMap} from './UserStoryMap.js';
 import {Home} from './Home.js'
+import {LeanCanvas} from './LeanCanvas.js'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Route exact path='/' component={Home} />
-      <Route path='/userstorymap' component={UserStoryMap} />    
+      <Route path='/userstorymap' component={UserStoryMap} /> 
+      <Route path='/leancanvas' component={LeanCanvas} />    
     </div>
   </BrowserRouter>
 )
---
+++ src/LeanCanvas.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './LeanCanvas.css'
+
+export class LeanCanvas extends Component {
+  render(){
+    return(
+      <div className='lean'>
+        <h1>LeanCanvasです</h1>    
+      </div>
+    )
+  }  
+}
