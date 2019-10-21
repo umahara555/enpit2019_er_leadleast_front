f54df6b8d03059ea927387b8977f961c3b1e4ea3
Ryutaro Osiro <e175739@ie.u-ryukyu.ac.jp>
2019-10-21 17:16:12 +0900

    ホームのレイアウトを調整しました

--- src/Home.js
+++ src/Home.js
@@ -5,31 +5,43 @@ import { Link } from 'react-router-dom';
 
 
 export class Home extends Component {
+  constructor(props){
+      super(props);
+   this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick(){
+    this.props.history.push("/userstorymap");
+  }
   render(){
     return(
       <div>
         <Header className='header' title={'LeadLeast'} />
         <div className='home'>
-       	  <p>プロダクトデザインを支援するツール</p>
-       	  <h1>
-       	     のHome画面です<br />
-       	     下記のリンクから目的のページへと移動できます。
-       	  </h1>  
-       	  <br /><br />
-       	  <Link to="/userstorymap">
+          <div className='terget'>
+       	    <h1>LeadLeast</h1>
+            <p>for beginner Agile team</p>
+            <br />
+              <button
+              className='Button'
+               onClick={this.handleClick}>
+               CLICK TO START
+               </button>
+          </div>
+        </div>
+        <div className='link'>
+        Link list
+          <Link to="/userstorymap">
             <p>・ホワイトボードへ</p>
-          </Link>  
+          </Link>
           <Link to="/leancanvas">
             <p>・リーンキャンパスへ</p>
-          </Link>   
+          </Link>
           <Link to="/login">
             <p>・ログイン画面へ</p>
-          </Link>  
+          </Link>
         </div>
-        <div>
-        </div>
-      </div>  
-        
+      </div>
+
     )
-  }  
+  }
 }
