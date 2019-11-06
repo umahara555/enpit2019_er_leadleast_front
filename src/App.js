a0d5d4dbeb8df8fcf7556adf6a02cb71a4bec8a9
Uehara Yuuku <e175732@ie.u-ryukyu.ac.jp>
2019-11-06 11:41:39 +0900

    url内のparams取得のサンプルコードを作成

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import {LeanCanvas} from './LeanCanvas.js'
 import {Login} from './Login.js'
 import {ElevatorPitch} from './elevatorPitch.js'
 import {ProductBacklog} from './ProductBacklog.js'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link, useParams} from 'react-router-dom';
 
 import { NewHeader } from './NewHeader.js'
 const NH = () => (<NewHeader isLoggedin={true}/>)
+
+function Lean() {
+  let { productID } = useParams();
+  return <p>productID : {productID}</p>;
+}
 
 const App = () => (
   <BrowserRouter>
@@ -21,6 +26,7 @@ const App = () => (
       <Route path='/productbacklog' component={ProductBacklog} />
       <Route path='/login' component={Login} />
       <Route path='/nh' component={NH} />
+      <Route path='/product/:productID/leancanvas' component={Lean} />
     </div>
   </BrowserRouter>
 )
