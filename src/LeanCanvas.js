9a6a9ad8d5364ce2d9aa8960fbbd23fb96db897f
Uehara Yuuku <e175732@ie.u-ryukyu.ac.jp>
2019-11-12 03:39:38 +0900

    space

--- src/LeanCanvas.js
+++ src/LeanCanvas.js
@@ -13,105 +13,105 @@ export class LeanCanvas extends Component {
   }
   
   guideFlagChange() {
-      this.setState({guideFlag: !this.state.guideFlag});
+    this.setState({guideFlag: !this.state.guideFlag});
   }
- render(){
-    return(    
+
+  render(){
+    return(
       <div>
-      { this.state.guideFlag && <GuideLeanCanvas onClick={() => this.guideFlagChange()} /> }
+        { this.state.guideFlag && <GuideLeanCanvas onClick={() => this.guideFlagChange()} /> }
         <Header className='header' title='リーンキャンバス' />
         <ShowGuide  onClick={() => this.guideFlagChange()} />
         <NextButton urlName="/product/0/elevatorpitch" />
         <div className='lean'>
-            <div className="kadai">
-                <textarea className="name" readOnly >
-                    [1]課題　
-                </textarea>
-                <textarea className="write" />
-            </div>
-            
-            <div className="kizon">
-                <textarea className="name" readOnly >
-                    [2]既存の代替品
-                </textarea>
-                <textarea className="write" />
-            </div>            
-            
-            <div className="solution">
-                <textarea className="name" readOnly >
-                    [7]ソリューション
-                </textarea>
-                <textarea className="write" />
-            </div>            
+          <div className="kadai">
+            <textarea className="name" readOnly >
+              [1]課題　
+            </textarea>
+            <textarea className="write" />
+          </div>
 
-            <div className="syuyou">
-                <textarea className="name" readOnly >
-                    [12]主要指標
-                </textarea>
-                <textarea className="write" />
-            </div>
+          <div className="kizon">
+            <textarea className="name" readOnly >
+              [2]既存の代替品
+            </textarea>
+            <textarea className="write" />
+          </div>
 
-            <div className="dokuzi">
-                <textarea className="name" readOnly >
-                    [5]独自の価値提案
-                </textarea>
-                <textarea className="write" />
-            </div>
-            
-            <div className="highlevel">
-                <textarea className="name" readOnly >
-                    [6]ハイレベルコンセプト
-                </textarea>
-                <textarea className="write" />
-            </div>            
+          <div className="solution">
+            <textarea className="name" readOnly >
+              [7]ソリューション
+            </textarea>
+            <textarea className="write" />
+          </div>
 
-            <div className="attouteki">
-                <textarea className="name" readOnly >
-                    [11]圧倒的な優位性
-                </textarea>
-                <textarea className="write" />
-            </div>            
+          <div className="syuyou">
+            <textarea className="name" readOnly >
+              [12]主要指標
+            </textarea>
+            <textarea className="write" />
+          </div>
 
-            <div className="tyaneru">
-                <textarea className="name" readOnly >
-                    [8]チャネル
-                </textarea>
-                <textarea className="write" />
-            </div>               
+          <div className="dokuzi">
+            <textarea className="name" readOnly >
+              [5]独自の価値提案
+            </textarea>
+            <textarea className="write" />
+          </div>
 
-            <div className="kokyaku">
-                <textarea className="name" readOnly >
-                    [3]顧客セグメント
-                </textarea>
-                <textarea className="write" />
-            </div>        
+          <div className="highlevel">
+            <textarea className="name" readOnly >
+              [6]ハイレベルコンセプト
+            </textarea>
+            <textarea className="write" />
+          </div>
 
-            <div className="early">
-                <textarea className="name" readOnly >
-                    [4]アーリーアダプタ
-                </textarea>
-                <textarea className="write" />
-            </div>                    
+          <div className="attouteki">
+            <textarea className="name" readOnly >
+              [11]圧倒的な優位性
+            </textarea>
+            <textarea className="write" />
+          </div>
 
-            <div className="cost">
-                <textarea className="name" readOnly >
-                    [10]コスト構造
-                </textarea>
-                <textarea className="write" />
-            </div>   
+          <div className="tyaneru">
+            <textarea className="name" readOnly >
+              [8]チャネル
+            </textarea>
+            <textarea className="write" />
+          </div>
 
-            <div className="syueki">
-                <textarea className="name" readOnly >
-                    [9]収益の流れ
-                </textarea>
-                <textarea className="write" />
-            </div>                                                                                 
-      <button onclick={()=>this.download}>ダウンロード</button>
+          <div className="kokyaku">
+            <textarea className="name" readOnly >
+              [3]顧客セグメント
+            </textarea>
+            <textarea className="write" />
+          </div>
 
-       </div>
-      </div>  
+          <div className="early">
+            <textarea className="name" readOnly >
+              [4]アーリーアダプタ
+            </textarea>
+            <textarea className="write" />
+          </div>
+
+          <div className="cost">
+            <textarea className="name" readOnly >
+              [10]コスト構造
+            </textarea>
+            <textarea className="write" />
+          </div>
+
+          <div className="syueki">
+            <textarea className="name" readOnly >
+              [9]収益の流れ
+            </textarea>
+          <textarea className="write" />
+        </div>
+        <button onclick={()=>this.download}>ダウンロード</button>
+      </div>
+    </div>
     )
-  }  
+  }
 }
 
 
