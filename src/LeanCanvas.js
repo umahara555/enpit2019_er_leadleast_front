e46a0687aa46d2c075c0688dcb856c66e72e6af2
HALtitor <e175753@ie.u-ryukyu.ac.jp>
2019-11-11 21:10:48 +0900

    リーンキャンバスのお題に番号を割り振った。ガイドを充実させた

--- src/LeanCanvas.js
+++ src/LeanCanvas.js
@@ -15,7 +15,7 @@ export class LeanCanvas extends Component {
   guideFlagChange() {
       this.setState({guideFlag: !this.state.guideFlag});
   }
-  render(){
+ render(){
     return(    
       <div>
       { this.state.guideFlag && <GuideLeanCanvas onClick={() => this.guideFlagChange()} /> }
@@ -25,76 +25,108 @@ export class LeanCanvas extends Component {
         <div className='lean'>
             <div className="kadai">
                 <textarea className="name" readOnly >
-                    課題
+                    [1]課題　
                 </textarea>
                 <textarea className="write" />
             </div>
             
             <div className="kizon">
                 <textarea className="name" readOnly >
-                    既存の代替品
+                    [2]既存の代替品
                 </textarea>
                 <textarea className="write" />
             </div>            
             
             <div className="solution">
                 <textarea className="name" readOnly >
-                    ソリューション
+                    [5]ソリューション
                 </textarea>
                 <textarea className="write" />
             </div>            
 
             <div className="syuyou">
                 <textarea className="name" readOnly >
-                    主要指標
+                    [9]主要指標
                 </textarea>
                 <textarea className="write" />
             </div>
 
             <div className="dokuzi">
                 <textarea className="nameL" readOnly >
-                    独自の価値提案
+                    [4]独自の価値提案
                 </textarea>
                 <textarea className="writeL" />
             </div>
 
             <div className="attouteki">
                 <textarea className="name" readOnly >
-                    圧倒的な優位性
+                    [10]圧倒的な優位性
                 </textarea>
                 <textarea className="write" />
             </div>            
 
             <div className="tyaneru">
                 <textarea className="name" readOnly >
-                    チャネル
+                    [6]チャネル
                 </textarea>
                 <textarea className="write" />
             </div>               
 
             <div className="kokyaku">
                 <textarea className="nameL" readOnly >
-                    顧客セグメント
+                    [3]顧客セグメント
                 </textarea>
                 <textarea className="writeL" />
             </div>        
 
             <div className="cost">
                 <textarea className="name" readOnly >
-                    コスト構造
+                    [8]コスト構造
                 </textarea>
                 <textarea className="write" />
             </div>   
 
             <div className="syueki">
                 <textarea className="name" readOnly >
-                    収益の流れ
+                    [7]収益の流れ
                 </textarea>
                 <textarea className="write" />
             </div>                                                                                 
+      <button onclick={()=>this.download}>ダウンロード</button>
 
        </div>
       </div>  
     )
   }  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
