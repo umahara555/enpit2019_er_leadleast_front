c7b762d7ec89f7630c099d324219fb8028bee0f7
Uehara Yuuku <e175732@ie.u-ryukyu.ac.jp>
2019-11-18 21:15:28 +0900

    エレベーターピッチの同期処理追加

--- src/ElevatorPitch.js
+++ src/ElevatorPitch.js
@@ -4,6 +4,7 @@ import { GuideElevatorPitch, ShowGuide, NextButton, BackButton, AllMenu } from '
 import './ElevatorPitch.css';
 
 const API_URL = 'http://localhost:5000/api/v1';
+const API_WS_URL = 'ws://localhost:5000/cable';
 
 export class ElevatorPitch extends Component {
 
@@ -21,10 +22,42 @@ export class ElevatorPitch extends Component {
 				txt6: { text: '', } ,
 				txt7: { text: '', } ,
 			},
+			ws: null,
     };
 		this.handleChange = this.handleChange.bind(this);
 		this.getBoardTexts();
   }
+
+	componentDidMount() {
+		const ws = new WebSocket(API_WS_URL);
+		ws.onopen = () => {
+			ws.send(
+				JSON.stringify(
+					{"command": "subscribe",
+						"identifier":"{\"channel\":\"ElevatorPitchChannel\"}"}
+				)
+			);
+		};
+		ws.onmessage = this.handleBoard.bind(this);
+		this.setState({ws: ws});
+	}
+
+	componentWillUnmount() {
+		this.state.ws.close();
+	}
+
+	handleBoard(event) {
+		const data = JSON.parse(event.data);
+		if ('message' in data) {
+			const messageData = JSON.parse(data.message);
+			if (typeof(messageData) == 'object' && 'board_texts' in messageData) {
+				this.setState({
+					board_texts: messageData.board_texts,
+				});
+				console.log(messageData.board_texts);
+			}
+		}
+	}
   
   guideFlagChange() {
       this.setState({guideFlag: !this.state.guideFlag});
