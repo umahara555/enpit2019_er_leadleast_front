222b2740573e2036e5309ca756169143eb3d5a67
HALtitor <e175753@ie.u-ryukyu.ac.jp>
2019-11-19 12:03:20 +0900

    バーの色変更

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -17,10 +17,8 @@ export class Tutorial extends Component {
   render(){
     return(
       <div>
-        <ShowGuide  onClick={() => null} />
         <Header className='header' title='トップ' />
         <NextButton urlName={"/product/" + this.props.match.params.productID + "/leancanvas"} />  
-        <BackButton />         
         <AllMenu className="allmenu-user" Tflag={true} TurlName={"/product/" + this.props.match.params.productID} LurlName={"/product/" + this.props.match.params.productID + "/leancanvas"} EurlName={"/product/" + this.props.match.params.productID + "/elevatorpitch"} UurlName={"/product/" + this.props.match.params.productID + "/userstorymap"} PurlName={"/product/" + this.props.match.params.productID + "/productbacklog"}/>      
         <div className="tutorial">
           <div className="beforecom">
@@ -65,7 +63,7 @@ export class Tutorial extends Component {
             </p>
                     <AllMenu/>
           <p className="flow">                    
-          は現在取り組んでいる作業を確認することができます。リーンキャンバスに取り組んでいるときは、<br/>バーのリーンキャンバスの部分がオレンジ色に変わります。現在はトップ画面なのでトップがオレンジ色になっています。<br/>
+          は現在取り組んでいる作業を確認することができます。リーンキャンバスに取り組んでいるときは、<br/>バーのリーンキャンバスの部分の色が変わります。現在はトップ画面なのでトップの色が他と異なっています。<br/>
           またバーはクリックすることができ、移動したい場所を選択すれば即座にその場へ飛ぶことができます。
           <br/><br/>
           <p className="emphaB">          
@@ -76,7 +74,7 @@ export class Tutorial extends Component {
            </p> <br/>
            はクリックすることで現在の作業場所から、プロダクトデザインの流れに沿って、前後の作業場所に飛ぶことができます。<br/>
            例えばユーザーストーリーマップの画面であるならば,<br/>Backでエレベーターピッチ,Nextでプロダクトバックログに飛ぶことができます。<br/>
-           このページにおいては、Backを押しても移動はしませんが、Nextを押せばリーンキャンバスへ移動できます。<br/><br/>
+           このページにおいては、Nextを押せばリーンキャンバスへ移動できます。<br/><br/>
           <p className="emphaB">           
            3.右上に設置されているGUIDEボタン↓
            </p>
@@ -84,8 +82,7 @@ export class Tutorial extends Component {
         <ShowGuide  onClick={() => null} />
             </p><br/>
             はクリックすることで現在の作業の取り組み方についての説明が表示されます。<br/>
-            例えばリーンキャンパスの画面でクリックすれば、リーンキャンバスの取り組み方の説明が表示されます。<br/>このページにおいてはクリックしても反応しません。
-                  
+            例えばリーンキャンパスの画面でクリックすれば、リーンキャンバスの取り組み方の説明が表示されます。<br/>                  
           </p>          
         </div>        
         </div>
