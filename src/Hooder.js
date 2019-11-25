06b633789f77c1d1b0063c6bedde46a4a768a88e
Ryutaro Osiro <e175739@ie.u-ryukyu.ac.jp>
2019-11-25 22:39:05 +0900

    レイアウトの調整とフッダーエリアの範囲調整

--- src/Hooder.js
+++ src/Hooder.js
@@ -6,7 +6,17 @@ export class Hooder extends Component {
   render(){
     return(
       <div className="Hooder-container-wrapper">
-        これはフッダーです
+        <div className='Hooder-container'>
+          <div className='box'>
+            <div className="hooder">
+              <Link to="/" className="hooder-home-btn">
+                <p>LeadLeast</p>
+              </Link>
+              <p>{this.props.title}</p>
+            </div>
+            これはHooderです。
+          </div>
+        </div>
       </div>
     )
   }
