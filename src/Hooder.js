3254634078301345143177ea3269144b1ec0c9bf
Ryutaro Osiro <e175739@ie.u-ryukyu.ac.jp>
2019-11-25 21:27:35 +0900

    フッダーのハリボテ作成

---
+++ src/Hooder.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './Hooder.css';
+
+export class Hooder extends Component {
+  render(){
+    return(
+      <div className="Hooder-container-wrapper">
+        これはフッダーです
+      </div>
+    )
+  }
+}
