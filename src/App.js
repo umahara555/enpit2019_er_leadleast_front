c9e3fbd167b2b59c35d5afd1a18214272fe48ed0
Yuto Uechi <e175706@ie.u-ryukyu.ac.jp>
2019-12-02 14:48:31 +0900

    App.jsをクラス化した

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './Home.js';
 import { LeanCanvas } from './LeanCanvas.js';
@@ -10,26 +10,30 @@ import { Tutorial } from './Tutorial.js'
 import './App.css';
 
 import { NewHeader } from './NewHeader.js'
+
 const NH = () => (<NewHeader isLoggedin={true}/>);
 
 const page404 = () => {
-  return <p>404 Not Found</p>;
+    return <p>404 Not Found</p>;
 };
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/product/:productID/' component={Tutorial} />
-      <Route exact path='/product/:productID/leancanvas' component={LeanCanvas} />
-      <Route exact path='/product/:productID/elevatorpitch' component={ElevatorPitch} />
-      <Route exact path='/product/:productID/userstorymap' component={UserStoryMap} />
-      <Route exact path='/product/:productID/productbacklog' component={ProductBacklog} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/nh' component={NH} />
-      <Route exact component={page404} />
-    </Switch>
-  </Router>
-);
-
+class App extends Component{
+    render() {
+      return (
+          <Router>
+              <Switch>
+                  <Route exact path='/' component={Home}/>
+                  <Route exact path='/product/:productID/' component={Tutorial}/>
+                  <Route exact path='/product/:productID/leancanvas' component={LeanCanvas}/>
+                  <Route exact path='/product/:productID/elevatorpitch' component={ElevatorPitch}/>
+                  <Route exact path='/product/:productID/userstorymap' component={UserStoryMap}/>
+                  <Route exact path='/product/:productID/productbacklog' component={ProductBacklog}/>
+                  <Route exact path='/login' component={Login}/>
+                  <Route exact path='/nh' component={NH}/>
+                  <Route exact component={page404}/>
+              </Switch>
+          </Router>
+      )
+  };
+}
 export default App;
